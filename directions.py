3427aca54c0166acb9276d6fa621e5a811a166bf
Mohd Safwan <kdbeatbox@gmail.com>
2019-01-27 17:31:55 +0530

    Add files via upload

---
+++ directions.py
@@ -0,0 +1,18 @@
+import requests
+import html2text
+
+h = html2text.HTML2Text(	)
+key = open('google-api-key', 'r')
+apiKey = key.read()
+
+origin = raw_input('Origin: ')
+destination = raw_input('Destination: ')
+
+url = 'https://maps.googleapis.com/maps/api/directions/json?origin='+origin+'&destination='+destination+'&key='+apiKey
+
+r = requests.get(url)
+data = r.json()['routes'][0]
+print('\n' + data['summary'])
+for legs in data['legs']:
+	for steps in legs['steps']:
+		print(h.handle(steps['html_instructions']))
